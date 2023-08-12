bb186a3347fa76d561e6bea1d5515a2ea1ff80ac
Jackson Buchmeyer <jackson.buchmeyer@drake.edu>
2023-08-11 19:07:20 -0500

    Initial commit

---
+++ src/main/java/net/jixson/jinventory/JInventoryClient.java
@@ -0,0 +1,10 @@
+package net.jixson.jinventory;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JInventoryClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/jixson/jinventory/JInventoryDataGenerator.java
@@ -0,0 +1,11 @@
+package net.jixson.jinventory;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class JInventoryDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
