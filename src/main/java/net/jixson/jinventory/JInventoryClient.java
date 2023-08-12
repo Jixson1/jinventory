ad690dbf5c84052d58cab803e4485f135ac15fb0
Jackson Buchmeyer <jackson.buchmeyer@drake.edu>
2023-08-12 17:13:16 -0500

    continue keybind testing

--- src/main/java/net/jixson/jinventory/JInventoryClient.java
+++ src/main/java/net/jixson/jinventory/JInventoryClient.java
@@ -5,23 +5,25 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.text.Text;
 import org.lwjgl.glfw.GLFW;
 
 public class JInventoryClient implements ClientModInitializer {
 
-    private static KeyBinding keyBinding;
+    private static KeyBinding toggleSlotLock;
     @Override
     public void onInitializeClient() {
-        keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-            "Favorite Item",
+        toggleSlotLock = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "Toggle Slot Lock",
             InputUtil.Type.KEYSYM,
-            GLFW.GLFW_KEY_EQUAL,
+            GLFW.GLFW_KEY_GRAVE_ACCENT,
             "JInventory"
         ));
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (keyBinding.wasPressed()) {
-                client.player.sendMessage(Text.literal("Favorite Item Key Pressed"), false);
+            while (toggleSlotLock.wasPressed()) {
+                client.player.sendMessage(Text.literal("Key pressed"), false);
+
             }
         });
     }
